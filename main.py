db94f4cc2a3963c59bd86a7d6a7f7d3ab6c864b9
Evan Lei <evanlei517@gmail.com>
2026-09-16 14:20:12 -0400

    Draw a creature and move it across the screen

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import pygame
 
 pygame.init()
 
-pygame.display.set_mode((1000, 700))
+screen = pygame.display.set_mode((1000, 700))
 pygame.display.set_caption("Evolution Simulator")
 
 running = True
+
+x = 500
+clock = pygame.time.Clock()
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    x += 1
+    screen.fill("black")
+    pygame.draw.circle(screen, "green", (x, 350), 10)
+    pygame.display.flip() # updates the screen
+    clock.tick(60)
 pygame.quit()
