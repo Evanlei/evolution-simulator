5cd7853acba24be99d27fadf4be4de791705dd95
Evan Lei <evanlei517@gmail.com>
2026-09-18 23:14:26 -0400

    added some directional movement and boundry conditionals

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ pygame.display.set_caption("Evolution Simulator")
 running = True
 
 x = 500
+y = 350
+velocity_x = 2
+velocity_y = 2
 clock = pygame.time.Clock()
 
 while running:
@@ -15,9 +18,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    x += 1
+    x += velocity_x
+    y += velocity_y
+    if x + 10 >= 1000 or x - 10 <= 0:
+        velocity_x = -velocity_x
+    if y + 10 >= 700 or y - 10 <= 0:
+        velocity_y = -velocity_y
+
     screen.fill("black")
-    pygame.draw.circle(screen, "green", (x, 350), 10)
+
+    pygame.draw.circle(screen, "green", (x, y), 10)
     pygame.display.flip() # updates the screen
     clock.tick(60)
+    
 pygame.quit()
